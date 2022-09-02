df33bdd9dba266f29a2cb4c4c45c911866f55d48
gotoworq <gotoworq@gmail.com>
2022-09-02 09:59:25 -0400

    added getProductsByCategory controller/route

--- app/controllers/products.controller.js
+++ app/controllers/products.controller.js
@@ -22,3 +22,32 @@ exports.getAllProducts = (req, res) => {
 
     })
 }
+
+exports.getProductsByCategory = (req, res) => {
+
+    const { category } = req.params
+
+    const script = `
+    SELECT *
+    FROM products
+    WHERE category = ?
+    `
+
+    const placeholderValues = [category];
+
+    db.query(script, placeholderValues, (err, results) => {
+        if (err) {
+            res.status(500).send({
+                error: err,
+                message: 'There was a problem finding this category, please try again'
+            })
+        } else if (results.length == 0) {
+            res.status(404).send({
+                error: err,
+                message: 'There was no data at this category, please input a valid category'
+            })
+        } else {
+            res.send(results)
+        }
+    })
+}
--- app/routes/products.routes.js
+++ app/routes/products.routes.js
@@ -3,5 +3,6 @@ module.exports = (app) => {
     const products = require('../controllers/products.controller');
 
     app.get('/api/products', products.getAllProducts);
+    app.get('/api/products/:category', products.getProductsByCategory)
 
 }
